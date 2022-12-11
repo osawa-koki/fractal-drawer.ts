925657f70c81c6fbb07a38d082dd8444a41763ae
osawa-koki <github@osawa.dev>
2022-12-11 22:13:49 +0900

    不要なファイル消した。

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,8 +17,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     <header>
       <nav>
         <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
       </nav>
     </header>
     {children}
