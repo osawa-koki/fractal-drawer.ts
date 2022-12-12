021c27e0e27896403b42c6d0366c201d94485dce
osawa-koki <github@osawa.dev>
2022-12-12 13:13:12 +0900

    不要な関数消した。

--- pages/buddhabrot.tsx
+++ pages/buddhabrot.tsx
@@ -28,47 +28,6 @@ const Buddhabrot = () => {
   let [numberOfTrials, setNumberOfTrials] = useState(100000);
   let [lighter, setLighter] = useState(0.1);
 
-  function hslToRgb(h: number, s: number, l: number) {
-    let r, g, b;
-    if (s == 0) {
-      r = g = b = l; // achromatic
-    } else {
-      let hue2rgb = function hue2rgb(p: number, q: number, t: number) {
-        if (t < 0) t += 1;
-        if (t > 1) t -= 1;
-        if (t < 1 / 6) return p + (q - p) * 6 * t;
-        if (t < 1 / 2) return q;
-        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-        return p;
-      }
-      let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      let p = 2 * l - q;
-      r = hue2rgb(p, q, h + 1 / 3);
-      g = hue2rgb(p, q, h);
-      b = hue2rgb(p, q, h - 1 / 3);
-    }
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-  }
-
-  function rgbToHsl(r: number, g: number, b: number) {
-    r /= 255, g /= 255, b /= 255;
-    let max = Math.max(r, g, b), min = Math.min(r, g, b);
-    let h, s, l = (max + min) / 2;
-    if (max == min) {
-      h = s = 0; // achromatic
-    } else {
-      let d = max - min;
-      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-      switch (max) {
-        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-        case g: h = (b - r) / d + 2; break;
-        case b: h = (r - g) / d + 4; break;
-      }
-      h /= 6;
-    }
-    return [h, s, l];
-  }
-
   // Buddhabrot
   function Draw() {
     if (canvasRef.current) {
