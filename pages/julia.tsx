21b20f75a1a30c2c89c3b1fbd5b0f44f1ec04473
osawa-koki <github@osawa.dev>
2022-12-12 12:27:21 +0900

    julia

--- pages/julia.tsx
+++ pages/julia.tsx
@@ -27,9 +27,36 @@ const Julia = () => {
   let [yMin, setYMin] = useState(-1.5);
   let [yMax, setYMax] = useState(1.5);
 
-  // drawing the Julia set
   function Draw() {
-    
+    let canvas = canvasRef.current;
+    if (canvas) {
+      let ctx = canvas.getContext('2d');
+      if (ctx) {
+        let xDelta = (xMax - xMin) / canvasSize;
+        let yDelta = (yMax - yMin) / canvasSize;
+        for (let x = 0; x < canvasSize; x++) {
+          for (let y = 0; y < canvasSize; y++) {
+            let z = {x: xMin + x * xDelta, y: yMin + y * yDelta};
+            let i = 0;
+            while (i < maxIterations) {
+              let z2 = {x: z.x * z.x - z.y * z.y, y: 2 * z.x * z.y};
+              z.x = z2.x + c.x;
+              z.y = z2.y + c.y;
+              if (z.x * z.x + z.y * z.y > threshold) {
+                break;
+              }
+              i++;
+            }
+            if (i == maxIterations) {
+              ctx.fillStyle = '#000000';
+            } else {
+              ctx.fillStyle = `hsl(${color + i * 360 / maxIterations}, 100%, 50%)`;
+            }
+            ctx.fillRect(x, y, 1, 1);
+          }
+        }
+      }
+    }
   }
 
   return (
@@ -62,6 +89,16 @@ const Julia = () => {
             <th>Threshold</th>
             <td><Form.Range min={thresholdMin} max={thresholdMax} value={threshold} onInput={(e) => {setThreshold(parseInt((e.target as HTMLInputElement).value))}} /></td>
             <td>{threshold}</td>
+          </tr>
+          <tr>
+            <th>C(x)</th>
+            <td><Form.Control type='number' step={axis_delta} value={c.x} onInput={(e) => {setC({x: parseFloat((e.target as HTMLInputElement).value), y: c.y})}} /></td>
+            <td>{c.x}</td>
+          </tr>
+          <tr>
+            <th>C(y)</th>
+            <td><Form.Control type='number' step={axis_delta} value={c.y} onInput={(e) => {setC({x: c.x, y: parseFloat((e.target as HTMLInputElement).value)})}} /></td>
+            <td>{c.y}</td>
           </tr>
           <tr>
             <th>X Min</th>
