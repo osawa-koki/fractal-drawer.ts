25d33351f639995c429e181d49db173214df557b
osawa-koki <github@osawa.dev>
2022-12-15 22:20:44 +0900

    コッホ曲線をタイムエフェクト設定した。

--- pages/koch-curve.tsx
+++ pages/koch-curve.tsx
@@ -17,6 +17,8 @@ const rangeMin = 0;
 const rangeMax = 100;
 const maxIterationMinCount = 3;
 const maxIterationMaxCount = 10;
+const timespanMin = 10;
+const timespanMax = 300;
 
 const divition = 3;
 
@@ -25,12 +27,13 @@ const KochCurve = () => {
   let canvasRef = useRef<HTMLCanvasElement>(null);
 
   let [canvasSize, setCanvasSize] = useState(300);
-  let [color, setColor] = useState(0);
+  let [color, setColor] = useState(200);
   let [ax, setAx] = useState(10);
-  let [ay, setAy] = useState(50);
+  let [ay, setAy] = useState(10);
   let [bx, setBx] = useState(90);
-  let [by, setBy] = useState(50);
+  let [by, setBy] = useState(90);
   let [maxIterations, setMaxIterations] = useState(5);
+  let [timespan, setTimespan] = useState(100);
   let [locked, setLocked] = useState(false);
 
   let canvas: HTMLCanvasElement;
@@ -40,7 +43,7 @@ const KochCurve = () => {
     canvas = canvasRef.current!;
     ctx = canvas.getContext('2d')!;
     Draw(false);
-  }, [canvasSize, color, ax, ay, bx, by, maxIterations]);
+  }, [canvasSize, color, ax, ay, bx, by, maxIterations, timespan]);
 
   useEffect (() => {
     canvas = canvasRef.current!;
@@ -51,16 +54,8 @@ const KochCurve = () => {
     if (locked) return;
     if (execute) setLocked(true);
     await new Promise(resolve => setTimeout(resolve, lock_affect_time));
-    function DrawOnCanvas(points: coord[]) {
-      ctx.beginPath();
-      ctx.moveTo(points[0].x, points[0].y);
-      for (let i = 1; i < points.length; i++) {
-        ctx.lineTo(points[i].x, points[i].y);
-      }
-      ctx.closePath();
-      ctx.stroke();
-    }
-    function GetMiddles(a: coord, b: coord): coord[] {
+    function CalcPoints(a: coord, b: coord, points: coord[], n: number) {
+      if (n === 0) return;
       const s: coord = {
         x: a.x + (b.x - a.x) / divition,
         y: a.y + (b.y - a.y) / divition,
@@ -73,31 +68,41 @@ const KochCurve = () => {
         x: s.x + (t.x - s.x) * Math.cos(Math.PI / 3) - (t.y - s.y) * Math.sin(Math.PI / 3),
         y: s.y + (t.x - s.x) * Math.sin(Math.PI / 3) + (t.y - s.y) * Math.cos(Math.PI / 3),
       };
-      return [s, t, u];
-    }
-    async function RecDraw(a: coord, b: coord, n: number) {
-      if (n <= 0) {
-        DrawOnCanvas([a, b]);
-        setLocked(false);
-        return;
-      }
-      const m = GetMiddles(a, b);
-      RecDraw(a, m[0], n - 1);
-      RecDraw(m[0], m[2], n - 1);
-      RecDraw(m[2], m[1], n - 1);
-      RecDraw(m[1], b, n - 1);
+      CalcPoints(a, s, points, n - 1);
+      points.push(s);
+      CalcPoints(s, u, points, n - 1);
+      points.push(u);
+      CalcPoints(u, t, points, n - 1);
+      points.push(t);
+      CalcPoints(t, b, points, n - 1);
     }
     ctx.clearRect(0, 0, canvasSize, canvasSize);
     ctx.strokeStyle = `hsl(${color}, 100%, 50%)`;
-    const a: coord = {
-      x: canvasSize * ax / rangeMax,
-      y: canvasSize - (canvasSize * ay / rangeMax),
-    };
-    const b: coord = {
-      x: canvasSize * bx / rangeMax,
-      y: canvasSize - (canvasSize * by / rangeMax),
-    };
-    RecDraw(a, b, execute ? maxIterations : 1);
+    ctx.lineWidth = 1;
+    const _ax = ax * canvasSize / 100;
+    const _ay = canvasSize - (ay * canvasSize / 100);
+    const _bx = bx * canvasSize / 100;
+    const _by = canvasSize - (by * canvasSize / 100);
+    if (execute === false) {
+      ctx.beginPath();
+      ctx.moveTo(_ax, _ay);
+      ctx.lineTo(_bx, _by);
+      ctx.stroke();
+      return;
+    }
+    for (let i = 1; i <= maxIterations; i++) {
+      ctx.clearRect(0, 0, canvasSize, canvasSize);
+      const points: coord[] = [];
+      CalcPoints({x: _ax, y: _ay}, {x: _bx, y: _by}, points, i);
+      ctx.beginPath();
+      ctx.moveTo(points[0].x, points[0].y);
+      for (let j = 1; j < points.length; j++) {
+        ctx.lineTo(points[j].x, points[j].y);
+      }
+      ctx.stroke();
+      await new Promise(resolve => setTimeout(resolve, timespan));
+    }
+    setLocked(false);
   }
 
   return (
@@ -146,6 +151,11 @@ const KochCurve = () => {
             <th>b.y</th>
             <td><Form.Range min={rangeMin} max={rangeMax} onInput={(e) => {setBy(parseInt((e.target as HTMLInputElement).value))}} /></td>
             <td>{by}</td>
+          </tr>
+          <tr>
+            <th>Timespan</th>
+            <td><Form.Range min={timespanMin} max={timespanMax} step={10} onInput={(e) => {setTimespan(parseInt((e.target as HTMLInputElement).value))}} /></td>
+            <td>{timespan}ms</td>
           </tr>
           <tr>
             <th>Max Iterations</th>
